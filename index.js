c7651460b1c55d26fb88a2c762afc168c680d0f7
Jatupon Jantri <jameball007@gmail.com>
2018-02-23 09:24:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -162,14 +162,17 @@ function moveDodger(e) {
     if (e.which === LEFT_ARROW) {
       moveDodgerLeft()
       e.preventDefault()
-      e.stop
+      e.stopPropagation()
       
       
     } else if (e.which === RIGHT_ARROW) {
       moveDodgerRight()
+      e.preventDefault()
+      e.stopPropagation()
       
     } else {
       e.preventDefault()
+      e.stopPropagation()
     }
   
 
